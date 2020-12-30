42520c97d3757398d76a3c4f25cb8c71f79aa3ee
abdallahshqair <42294488+abdallahshqair@users.noreply.github.com>
2020-12-30 22:33:30 +0200

    Authorization by Passport lgoin

--- routes/api.php
+++ routes/api.php
@@ -18,4 +18,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::apiResource('student','App\Http\Controllers\Api\StudentController');
+
+Route::post('login','App\Http\Controllers\Api\AuthController@login');
+
+Route::middleware('auth:api')->group(function (){
+    Route::apiResource('student','App\Http\Controllers\Api\StudentController');
+
+});
