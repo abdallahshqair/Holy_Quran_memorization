b9a7bc6ec80883ff5ee2fc0f10ce535bf5907394
abdallahshqair <42294488+abdallahshqair@users.noreply.github.com>
2020-12-30 21:36:42 +0200

    api student

--- app/Models/Student.php
+++ app/Models/Student.php
@@ -7,5 +7,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class Student extends Model
 {
+    protected $fillable=['st_name','st_father_phone','st_mother_phone','st_identity','st_date_of_birth'];
     use HasFactory;
 }
